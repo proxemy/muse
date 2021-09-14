ccd5a8a733512dfa8668972ffc09f4f0839c97a2
Proxemy <33573115+proxemy@users.noreply.github.com>
2021-09-14 13:08:58 +0200

    Add. Reading supported file formats from 'librosa' and passing it down to '__doc__' and 'glob_music_files'.

--- music_feature_extract.py
+++ music_feature_extract.py
@@ -1,43 +1,63 @@
 #!/usr/bin/env python3
 
-
-__supported_formats__ = [ "mp3", "ogg", "flac", "wav" ]
-
-__doc__ = \
-	"This script takes all audio files, either singular or recursive " \
-	"if a directory is passed and generates the several music feature extracts " \
-	"for the model to train on. " \
-	"Supported formats are {}".format(str(__supported_formats__))
-
-import argparse
 import pathlib
 
 from pdb import set_trace as BP
 
 
+def supported_formats():
+	from soundfile import available_formats as sf_avail_fmts
+
+	# mp3 is supported via librosas audioread dependecy
+	return sorted([ *sf_avail_fmts().keys(), "MP3" ])
+
+
+__doc__ = \
+	"""
+	This script takes all audio files, either singular or recursive
+	if a directory is passed and generates the several music feature extracts
+	for the model to train on.
+	Supported formats: [ {} ]
+	""".format(", ".join(supported_formats()))
+
+
+##################
+# business logic #
+##################
+
+
 def glob_music_files(input_pathes):
 	"""
 	If the 'input_pathes' list contains folders, these folders will recursively
-	searched for all supported formats: '{}'
-	""".format(__supported_formats__)
+	searched for all supported formats: '[ {} ]'
+	""".format(", ".join(supported_formats()))
 
-	ret = []
+	ret = set()
 
 	for p in input_pathes:
 		if not p.exists():
 			raise ValueError(f"'{p}' input path does not exist.")
 
 		if p.is_file():
-			ret.append(p)
+			if p.suffix[1:].upper() in supported_formats():
+				ret.update({p})
+			else:
+				raise ValueError(f"'{p}' input file format is not supported.")
+
 		elif p.is_dir():
-			for f in __supported_formats__:
-				ret.extend(p.rglob("*." + f))
+			ret.update([
+				f for f in p.rglob("*")
+				if f.suffix[1:].upper() in supported_formats()
+			])
+
+	if not ret:
+		raise ValueError(f"Could't find supported audio files at '{input_pathes}'")
 
 	return ret
 
 
-
 if __name__ == "__main__":
+	import argparse
 
 	arg_parser = argparse.ArgumentParser(description=__doc__)
 
@@ -47,13 +67,13 @@ if __name__ == "__main__":
 			"type" : pathlib.Path,
 			"required" : True,
 			"action" : "append",
-			"help" : "A single music file or a path for recursive lookup. (multiple)"
+			"help" : "A single music file or path for recursive lookup. (multiple)"
 		}),
 		( "-o", {
 			"dest" : "output_path",
 			"type" : pathlib.Path,
 			"default" : pathlib.Path().cwd(),
-			"help" : "Output folder. New directory will be created there."
+			"help" : "Output folder. New directories will be created there."
 		}),
 	]:
 		arg_parser.add_argument(arg[0], **arg[1])
