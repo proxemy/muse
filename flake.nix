a553a77e379265e424413e87f063233610606545
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-10-20 20:55:06 +0200

    Replaced shell.nix with flake.nix.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      #packages = forAllSystems (system: {
+        #default = pkgs.${system}.poetry2nix.mkPoetryApplication { projectDir = self; };
+      #});
+
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}.python3Packages; [
+			librosa
+			numpy
+			matplotlib
+          ];
+        };
+      });
+    };
+}
