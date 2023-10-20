5e07a2c26d2dbf1ff16911073b92e84bd82bb8fe
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-10-20 16:37:01 +0200

    Adjusted test script to iterable MFE class.

--- src/test.py
+++ src/test.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-
-# Beat tracking example
 import librosa
 from librosa_extractor import MFE
 
-
-mfe = MFE(librosa.example('nutcracker'))
-
+mfe = MFE(librosa.example('nutcracker', hq=True))
 
 print('Estimated tempo: {:.2f} beats per minute'.format(mfe.beat_tempo))
 
-
-# 4. Convert the frame indices of beat events into timestamps
 beat_times = librosa.frames_to_time(mfe.beat_frames, sr=mfe.sample_rate)
+
+for f in mfe:
+	print(f)
