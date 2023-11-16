b472a01a9eb540c9c769c94d605901b764325fa5
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-11-16 16:32:33 +0100

    WOP. 2 buttons, do not work.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, Button};
 
 
 const APP_ID: &str = env!("CARGO_PKG_NAME");
@@ -16,9 +16,34 @@ fn main() -> glib::ExitCode
 
 fn build_ui(app: &Application)
 {
+    let button1 = Button::builder()
+        .label("Process")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button2 = Button::builder()
+        .label("Process")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button1.connect_clicked(|button| {
+        button.set_label("new label");
+    });
+    button1.connect_clicked(|button| {
+        button.set_label("next label play");
+    });
+
     let window = ApplicationWindow::builder()
         .application(app)
         .title(env!("CARGO_PKG_NAME"))
+        .child(&button1)
+        .child(&button2)
         .build();
 
     window.present();
