3790394f1efa236f23e29f4539cad30726eb1c34
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-11-16 13:50:32 +0100

    First gui hello world operational.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "TODO: APP_ID";
+
+fn main() -> glib::ExitCode
+{
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+
+fn build_ui(app: &Application)
+{
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("TODO: title macro accesible?")
+        .build();
+
+    window.present();
 }
