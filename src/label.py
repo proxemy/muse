061a3d7b28015c0c12ab0253bbbe214388d1e941
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-11-16 17:18:28 +0100

    Added example GUI stuff.

--- src/label.py
+++ src/label.py
@@ -18,4 +18,26 @@ if __name__ == '__main__':
 	)
 	button.pack()
 
+	entry = tk.Entry(
+		fg="yellow",
+		bg="blue",
+		width=50
+	)
+	entry.pack()
+
+	text = tk.Text()
+	text.pack()
+
+	frame_a = tk.Frame()
+	frame_b = tk.Frame()
+
+	label_a = tk.Label(master=frame_a, text="I'm in Frame A")
+	label_a.pack()
+
+	label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+	label_b.pack()
+
+	frame_a.pack()
+	frame_b.pack()
+
 	window.mainloop()
